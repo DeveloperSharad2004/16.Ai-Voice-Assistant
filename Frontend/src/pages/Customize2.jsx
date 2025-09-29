d46e19b159078707683e6faf481d1c12d4cc46b2
Sharad Jaiswal <167406103+DeveloperSharad2004@users.noreply.github.com>
2025-09-29 17:35:34 +0530

    Update Customize2.jsx

--- Frontend/src/pages/Customize2.jsx
+++ Frontend/src/pages/Customize2.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { useState } from 'react'
-import { userDataContext } from '../context/userContext'
+import { userDataContext } from '../context/UserContext'
 import axios from 'axios';
 import { MdKeyboardBackspace } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
