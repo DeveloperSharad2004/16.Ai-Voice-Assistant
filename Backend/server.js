88931edb9a4bfa664cb7d61210463c0d4067257e
Sharad Jaiswal <167406103+DeveloperSharad2004@users.noreply.github.com>
2025-09-30 11:07:07 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ import geminiResponse from './gemini.js';
 
 const app = express()
 app.use(cors({
-    origin:"https://ai-voice-assistant-wekv.onrender.com",
+    origin:"https://ai-voice-assistant-frontend-4lty.onrender.com",
     credentials:true
 }));
 const port = process.env.PORT || 5000
